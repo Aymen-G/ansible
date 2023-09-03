7d69613b4ac60fb3eba60b814420d0f126318b5d
Aymen-G <aymen.gazzah@i-carre.net>
2023-09-03 22:20:56 +0200

    add v3 ansible as root

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update && \
     apt-get -y upgrade && \
-    apt-get install -y --no-install-recommends ansible sshpass git cron && \
+    apt-get install -y --no-install-recommends ansible sshpass && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
@@ -15,7 +15,7 @@ COPY ansible.cfg /etc/ansible/
 
 RUN chmod 777 -R /opt
 
-
+WORKDIR /home/root/ansible/
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ function display_error {
 # Default values
 DEFAULT_ANSIBLE_USERNAME="ansible"
 DEFAULT_ANSIBLE_VAULT_PASSWORD=supersecret
-DEFAULT_ANSIBLE_PRIV_KEY="/etc/hosts"
 
 # Check if required environment variables are set
 echo "######################"
@@ -17,26 +16,26 @@ echo "Checking docker environement values:"
 [ -n "$ANSIBLE_USERNAME" ] ||  display_error "ANSIBLE_USERNAME is not set, Using default: $DEFAULT_ANSIBLE_USERNAME"  || ANSIBLE_USERNAME=$DEFAULT_ANSIBLE_USERNAME
 if [ -e /id_rsa ]; then
     ANSIBLE_PRIV_KEY="/id_rsa"
+    echo \$ANSIBLE_USERNAME = $ANSIBLE_USERNAME
+    echo \$ANSIBLE_PRIV_KEY = $ANSIBLE_PRIV_KEY
+    echo \$ANSIBLE_VAULT_PASSWORD = $ANSIBLE_VAULT_PASSWORD
 else
-    display_error "/id_rsa not found, Using default: $DEFAULT_ANSIBLE_PRIV_KEY"
-    ANSIBLE_PRIV_KEY=$DEFAULT_ANSIBLE_PRIV_KEY
+    display_error "/id_rsa not found, deleting private_key_file arg in /etc/ansible/ansible.cfg"
+    sed -i '/^private_key_file/d' /etc/ansible/ansible.cfg
 fi
-export ANSIBLE_PRIV_KEY
-
-echo \$ANSIBLE_USERNAME = $ANSIBLE_USERNAME
-echo \$ANSIBLE_PRIV_KEY = $ANSIBLE_PRIV_KEY
 echo "######################"
 
 echo ""
 
-# Vérifie si le fichier /etc/ansible/ansible.cfg existe
+# Configuration Ansible
 if [ -f /etc/ansible/ansible.cfg ]; then
-    sed -i "s#^vault_password_file = .*#vault_password_file = $ANSIBLE_VAULT_PASSWORD#" /etc/ansible/ansible.cfg
     sed -i "s#^remote_user = .*#remote_user = $ANSIBLE_USERNAME#" /etc/ansible/ansible.cfg
     sed -i "s#^private_key_file = .*#private_key_file = $ANSIBLE_PRIV_KEY#" /etc/ansible/ansible.cfg
-else
-    display_error "Le fichier /etc/ansible/ansible.cfg n'existe pas."
+    echo "$ANSIBLE_VAULT_PASSWORD" > /etc/ansible/.vault_password
 fi
+
+echo "######################"
+echo "ansible.cfg:"
 
 cat /etc/ansible/ansible.cfg 
 
